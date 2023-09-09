b87b467f01d57f3520b2ea638c5ee6479f319661
Marlo Tunggolh <marlotunggolh@gmail.com>
2023-09-09 11:15:12 +0800

    Add block & inline section

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 const navTabs = document.querySelector(".nav-tabs");
+const blockInlineContainer = document.querySelector("#block-inline");
+const sample1 = document.querySelector(".sample-1");
 
-navTabs.addEventListener("click", function (e) {
+navTabs.addEventListener("click", (e) => {
   if (e.target && e.target.classList.contains("nav-link")) {
     e.preventDefault();
 
@@ -20,12 +22,27 @@ navTabs.addEventListener("click", function (e) {
 
     const contentItemId = e.target.getAttribute("data-target");
 
-    console.log(contentItemId);
-
     const contentItem = document.querySelector(`.${contentItemId}`);
 
     if (contentItem) {
       contentItem.classList.add("active");
     }
+  }
+});
+
+// BLOCK & INLINE
+blockInlineContainer.addEventListener("click", (e) => {
+  if (e.target.classList.contains("btn")) {
+    e.preventDefault();
+
+    const activeButton = blockInlineContainer.querySelector(".btn-primary");
+    activeButton.classList.replace("btn-primary", "btn-secondary");
+
+    e.target.classList.replace("btn-secondary", "btn-primary");
+
+    const targetClass = e.target.getAttribute("data-class");
+
+    sample1.classList.remove("block", "inline-block", "inline");
+    sample1.classList.add(targetClass);
   }
 });
