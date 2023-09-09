f563684ea7c600e7ba009dc28fbd22eab579e0f4
Marlo Tunggolh <marlotunggolh@gmail.com>
2023-09-09 14:55:08 +0800

    Add grid section

--- js/app.js
+++ js/app.js
@@ -8,8 +8,12 @@ const sample1 = document.querySelector(".sample-1");
 const flexDirectionBtnContainer = document.querySelector("#flex-direction");
 const flexJustifyBtnContainer = document.querySelector("#justify-content");
 const flexAlignBtnContainer = document.querySelector("#align-items");
-
 const sample2Container = document.querySelector(".content-sample-flex");
+
+// GRID
+const gridColBtnContainer = document.querySelector("#grid-columns");
+const gridRowBtnContainer = document.querySelector("#grid-rows");
+const sample3Container = document.querySelector(".content-sample-grid");
 
 const changeActiveButton = (btnContainer, currentActiveBtn) => {
   const activeBtn = btnContainer.querySelector(".btn-primary");
@@ -53,50 +57,76 @@ navTabs.addEventListener("click", (e) => {
 });
 
 // BLOCK & INLINE
-blockInlineContainer.addEventListener("click", (e) => {
+// blockInlineContainer.addEventListener("click", (e) => {
+//   if (e.target.classList.contains("btn")) {
+//     e.preventDefault();
+
+//     changeActiveButton(blockInlineContainer, e.target);
+
+//     changeElClass(e.target, sample1, ["block", "inline-block"]);
+//   }
+// });
+
+// flexDirectionBtnContainer.addEventListener("click", (e) => {
+//   if (e.target.classList.contains("btn")) {
+//     e.preventDefault();
+
+//     changeActiveButton(flexDirectionBtnContainer, e.target);
+//     changeElClass(e.target, sample2Container, ["flex-row", "flex-column"]);
+//   }
+// });
+
+// flexJustifyBtnContainer.addEventListener("click", (e) => {
+//   if (e.target.classList.contains("btn")) {
+//     e.preventDefault();
+
+//     changeActiveButton(flexJustifyBtnContainer, e.target);
+//     changeElClass(e.target, sample2Container, [
+//       "flex-justify-start",
+//       "flex-justify-center",
+//       "flex-justify-end",
+//       "flex-space-around",
+//       "flex-space-between",
+//       "flex-space-evenly",
+//     ]);
+//   }
+// });
+
+// flexAlignBtnContainer.addEventListener("click", (e) => {
+//   if (e.target.classList.contains("btn")) {
+//     e.preventDefault();
+
+//     changeActiveButton(flexAlignBtnContainer, e.target);
+//     changeElClass(e.target, sample2Container, [
+//       "align-center",
+//       "align-start",
+//       "align-end",
+//     ]);
+//   }
+// });
+
+gridColBtnContainer.addEventListener("click", (e) => {
   if (e.target.classList.contains("btn")) {
     e.preventDefault();
 
-    changeActiveButton(blockInlineContainer, e.target);
-
-    changeElClass(e.target, sample1, ["block", "inline-block"]);
-  }
-});
-
-flexDirectionBtnContainer.addEventListener("click", (e) => {
-  if (e.target.classList.contains("btn")) {
-    e.preventDefault();
-
-    changeActiveButton(flexDirectionBtnContainer, e.target);
-    changeElClass(e.target, sample2Container, ["flex-row", "flex-column"]);
-  }
-});
-
-flexJustifyBtnContainer.addEventListener("click", (e) => {
-  if (e.target.classList.contains("btn")) {
-    e.preventDefault();
-
-    changeActiveButton(flexJustifyBtnContainer, e.target);
-    changeElClass(e.target, sample2Container, [
-      "flex-justify-start",
-      "flex-justify-center",
-      "flex-justify-end",
-      "flex-space-around",
-      "flex-space-between",
-      "flex-space-evenly",
+    changeActiveButton(gridColBtnContainer, e.target);
+    changeElClass(e.target, sample3Container, [
+      "grid-col--1",
+      "grid-col--2",
+      "grid-col--3",
     ]);
   }
 });
 
-flexAlignBtnContainer.addEventListener("click", (e) => {
+gridRowBtnContainer.addEventListener("click", (e) => {
   if (e.target.classList.contains("btn")) {
     e.preventDefault();
 
-    changeActiveButton(flexAlignBtnContainer, e.target);
-    changeElClass(e.target, sample2Container, [
-      "align-center",
-      "align-start",
-      "align-end",
+    changeActiveButton(gridRowBtnContainer, e.target);
+    changeElClass(e.target, sample3Container, [
+      "grid-row--1",
+      "grid-row--2",
+      "grid-row--3",
     ]);
   }
 });
